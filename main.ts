e7b6e2f0de3af3ec221b62ef7b247d598651f293
Alexander <alexandermeier2020@outlook.de>
2025-01-11 20:01:21 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    startwert += 1
+    basic.showNumber(startwert)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    while (startwert != 0) {
+        basic.pause(1000)
+        startwert += -1
+        basic.showNumber(startwert)
+    }
+})
+let startwert = 0
+startwert = 1
+basic.showNumber(startwert)
