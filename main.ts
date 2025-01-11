5e4e364c98ab771cdad9bead9216dc55250f8019
Alexander <alexandermeier2020@outlook.de>
2025-01-11 20:05:01 +0100

    Jetzt verkleinert sich die Zahl durch Knopfdruck auf die Taste B; der Countdown startet mit Druck auf die Taste A+B

--- main.ts
+++ main.ts
@@ -2,12 +2,16 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     startwert += 1
     basic.showNumber(startwert)
 })
-input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
     while (startwert != 0) {
         basic.pause(1000)
         startwert += -1
         basic.showNumber(startwert)
     }
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    startwert += -1
+    basic.showNumber(startwert)
 })
 let startwert = 0
 startwert = 1
